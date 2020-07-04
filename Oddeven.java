b9dbc71b065a4f74ed7ca48d1db3b4255400a6ea
yugeshae <64422119+yugeshae@users.noreply.github.com>
2020-07-04 13:20:07 +0530

    Create Oddeven.java

---
+++ Oddeven.java
@@ -0,0 +1,16 @@
+package wipro;
+
+public class Oddeven {
+
+	public static void main(String[] args) {
+		int a=21;
+		if(a%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+
+	}
+
+}
