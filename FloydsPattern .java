4e3e75837c7b31342e7f96db1ac2054890fdb30f
yugeshae <64422119+yugeshae@users.noreply.github.com>
2020-07-04 19:30:31 +0530

    Create FloydsPattern .java

---
+++ FloydsPattern .java
@@ -0,0 +1,21 @@
+package myproject;
+
+import java.util.Scanner;
+
+public class FloydsPattern {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n;
+		n=s.nextInt();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println("\n");
+		}
+		
+
+	}
+
+}
