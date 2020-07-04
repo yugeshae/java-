11d256b81e716295f120143ebc10ba8b77f3943e
yugeshae <64422119+yugeshae@users.noreply.github.com>
2020-07-04 13:17:32 +0530

    Create Lastdigitsame.java

---
+++ Lastdigitsame.java
@@ -0,0 +1,20 @@
+package wipro;
+
+public class Lastdigitsame {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=77;
+		b=68;
+		a=a%10;
+		b=b%10;
+		if(a==b) {
+			System.out.println("true");
+		}
+		else {
+			System.out.println("false");
+		}
+
+	}
+
+}
