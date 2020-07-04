8f5b7b2281b50c36b843ec179e01ee317c531a3d
yugeshae <64422119+yugeshae@users.noreply.github.com>
2020-07-04 19:46:59 +0530

    Create FlowControlGender.java

---
+++ FlowControlGender.java
@@ -0,0 +1,26 @@
+package wipro;
+
+public class FlowControlGender {
+
+	public static void main(String[] args) {
+		String s1="Male";
+		String s2="Female";
+		double interest;
+		int a=Integer.parseInt(args[1]);
+		if(s2.equals(args[0]) && (a>=1 && a<58)){
+			 interest=8.4;
+			 System.out.println(interest);
+		}
+		else if(s2.equals(args[0]) && (a>=59 && a<100)) {
+			interest=9.2;
+			System.out.println(interest);
+		}
+		else if(s1.equals(args[0]) && (a>=1 && a<58)) {
+			interest=8.4;
+			System.out.println(interest);
+		}
+		else {
+			interest=10.5;
+			System.out.println(interest);
+		}
+	}
