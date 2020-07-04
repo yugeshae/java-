a136a0c70b3e2d34bb9dc9428c19c26c5d6dfb0a
yugeshae <64422119+yugeshae@users.noreply.github.com>
2020-07-04 16:43:27 +0530

    Create Colorswitch.java

---
+++ Colorswitch.java
@@ -0,0 +1,19 @@
+package wipro;
+
+public class Colorswitch {
+
+	public static void main(String[] args) {
+		char color='y';
+		switch(color) {
+		case 'R':System.out.println("RED");break;
+		case 'B':System.out.println("BLUE");break;
+		case 'G':System.out.println("GREEN");break;
+		case 'O':System.out.println("ORANGE");break;
+		case 'Y':System.out.println("YELLOW");break;
+		case 'W':System.out.println("WHITE");break;
+		default:System.out.println("Invalid Code");
+		}
+
+	}
+
+}
