f54aaa686791525abf0e58d8440b2210d6809e75
yugeshae <64422119+yugeshae@users.noreply.github.com>
2020-07-04 09:22:46 +0530

    Create Example2.java

---
+++ Example2/Example2.java
@@ -0,0 +1,10 @@
+package wipro;
+
+public class Example2{
+	public static void main(String[] args){
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int sum = a + b;
+		System.out.println("The sum of " + a + " and " + b +"is" +sum);
+		}
+		}
